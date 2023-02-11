8ea5cdea7b6a17cc032b731a97d8393589bbad3f
Tiago da Silva <tiagopan101@gmail.com>
2023-02-10 21:13:13 -0400

    validando formulario de cadastro de novo usuario

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,16 +1,56 @@
 /* eslint-disable react/jsx-no-bind */
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { get } from 'lodash';
 
 import { Form } from './styled';
 import { Container } from '../../styles/globalStyles';
+
+import axios from '../../services/axios';
+import history from '../../services/history';
 
 export default function Register() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error('O nome deve ter entre 3 e 255 caracteres!');
+    }
+
+    if (!isEmail(email)) {
+      // eslint-disable-next-line no-unused-vars
+      formErrors = true;
+      toast.error('Digite um email valido!');
+    }
+
+    if (password.length < 3 || password.length > 255) {
+      // eslint-disable-next-line no-unused-vars
+      formErrors = true;
+      toast.error('A senha deve ter entre 6 e 50 caracteres!');
+    }
+
+    if (formErrors) return;
+
+    try {
+      await axios.post('/users/', {
+        nome,
+        email,
+        password,
+      });
+      toast.success('Voce fez seu cadastro!');
+      history.push('/login');
+    } catch (err) {
+      const errors = await get(err, 'response.data.Errors', []);
+
+      errors.map((error) => toast.error(error));
+    }
   }
 
   return (
@@ -29,7 +69,7 @@ export default function Register() {
         <label htmlFor="email">
           E-mail:
           <input
-            type="text"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Seu e-mail"
@@ -38,7 +78,7 @@ export default function Register() {
         <label htmlFor="password">
           Senha:
           <input
-            type="text"
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Sua nova senha"
